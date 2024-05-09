96345ddf367b170ecdf9700cc18c1e3444d4148b
sahithyajadala <169046012+sahithyajadala@users.noreply.github.com>
2024-05-09 11:58:13 +0530

    Update webcame.py

--- webcame.py
+++ webcame.py
@@ -1,23 +1,20 @@
 import cv2
-# define a video capture object 
-vid = cv2.VideoCapture(0) 
-  
-while(True): 
-      
-    # Capture the video frame 
-    # by frame 
-    ret, frame = vid.read() 
-  
-    # Display the resulting frame 
-    cv2.imshow('frame', frame) 
-      
-    # the 'q' button is set as the 
-    # quitting button you may use any 
-    # desired button of your choice 
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
+
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+size = (width, height)
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('web_video.mp4', fourcc, 20.0, size)
+
+while(True):
+    _, frame = cap.read()
+    cv2.imshow('Recording...', frame)
+    out.write(frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-  
-# After the loop release the cap object 
-vid.release() 
-# Destroy all the windows 
+
+cap.release()
+out.release()
 cv2.destroyAllWindows()
+
